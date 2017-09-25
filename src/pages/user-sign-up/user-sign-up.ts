005713259d309bc79d3abf44f976c302d46e58f7
Anne Barrett <anneb1397@yahoo.com>
2017-09-24 23:46:35 -0500

    Home page altered, more functionality
    Joining, creating groups functional. Spot to create user on home page.
    MUST DO THIS FIRST FOR CORRECT BEHAVIOR. Things to implement: better ui,
    navigation, submission verification.

---
+++ src/pages/user-sign-up/user-sign-up.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the UserSignUpPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-user-sign-up',
+  templateUrl: 'user-sign-up.html',
+})
+export class UserSignUpPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad UserSignUpPage');
+  }
+
+}
